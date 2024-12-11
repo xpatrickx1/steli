bb79e8198bed2dab8c1ac035aef48eba9906f0b2
dev <xpatrickx1@gmail.com>
2024-12-12 00:41:17 +0200

    beginin5

--- pages/page-front.php
+++ pages/page-front.php
@@ -13,8 +13,13 @@ get_header();
 <?php get_template_part('includes/sections/page-front/join')?>
 <?php get_template_part('includes/sections/page-front/call')?>
 <?php get_template_part('includes/sections/page-front/lighting')?>
+<?php get_template_part('includes/sections/page-front/design')?>
+<?php get_template_part('includes/sections/page-front/banner')?>
+<?php get_template_part('includes/sections/page-front/quality')?>
+<?php get_template_part('includes/sections/page-front/numbers')?>
+<?php get_template_part('includes/sections/page-front/benefits')?>
+<?php get_template_part('includes/sections/page-front/examples')?>
 
-<?php get_template_part('includes/sections/page-front/book')?>
 <?php get_template_part('includes/sections/page-front/phrase')?>
 <?php get_template_part('includes/sections/page-front/about')?>
 <?php get_template_part('includes/sections/page-front/reviews')?>
