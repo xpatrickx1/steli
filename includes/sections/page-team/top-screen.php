71b6dc48ab130a20a68e67d5c1844571a24c7630
dev <xpatrickx1@gmail.com>
2025-01-10 15:15:55 +0200

    fix 3

--- includes/sections/page-team/top-screen.php
+++ includes/sections/page-team/top-screen.php
@@ -22,61 +22,62 @@
     </div>
   </div>
   
-  <div class="container top-screen__wrap">
-
-    <div class="top-screen--top">
-      <h2>
-        <?= get_field( 'page_title' ) ? the_field( 'page_title' ) : get_the_title() ?>
-      </h2>
-    
-      <ul class="top-screen__list">
-        <?php if (have_rows('top_list')) :
-          while ( have_rows('top_list')) : the_row(); ?>
-            <li class="item"><?= get_sub_field('item_text') ?></li>
-          <?php endwhile; ?>
-        <?php else : ?>
-          <?php foreach ( $order as $key => $item ) : ?>
-            <li class="item"><?= $item['text'] ?> </li>
-          <?php endforeach; ?>
-        <?php endif; ?>
-      </ul>
-    </div>
-
-    <div class="top-screen--bottom">
-      <div class="top-screen__images--top">
-        <img 
-          src="<?= bloginfo('template_url') . '/images/loader.gif' ?>" 
-          data-src="<?= get_sub_field('team_top_1')['url'] ? get_sub_field('team_top_1')['url'] : bloginfo('template_url') . '/images/page-team/team1.webp' ?>"
-          class="lazy"
-          width="1px"
-          height="1px"
-        />
-      </div>
-      <div class="top-screen__images--bottom">
-        
-        <img 
-          src="<?= bloginfo('template_url') . '/images/loader.gif' ?>" 
-          data-src="<?= get_sub_field('team_top_2')['url'] ? get_sub_field('team_top_2')['url'] : bloginfo('template_url') . '/images/page-team/team2.webp' ?>"
-          class="lazy"
-          width="1px"
-          height="1px"
-        />
-        <img 
-          src="<?= bloginfo('template_url') . '/images/loader.gif' ?>" 
-          data-src="<?= get_sub_field('team_top_3')['url'] ? get_sub_field('team_top_3')['url'] : bloginfo('template_url') . '/images/page-team/team3.webp' ?>"
-          class="lazy"
-          width="1px"
-          height="1px"
-        />
-        <img 
-          src="<?= bloginfo('template_url') . '/images/loader.gif' ?>" 
-          data-src="<?= bloginfo('template_url') . '/images/page-team/team4.webp' ?>"
-          class="lazy"
-          width="1px"
-          height="1px"
-        />
-      </div>
+  <div class="container ">
+    <div class="top-screen__wrap">
+      <div class="top-screen--top">
+        <h2>
+          <?= get_field( 'page_title' ) ? the_field( 'page_title' ) : get_the_title() ?>
+        </h2>
       
+        <ul class="top-screen__list">
+          <?php if (have_rows('top_list')) :
+            while ( have_rows('top_list')) : the_row(); ?>
+              <li class="item"><?= get_sub_field('item_text') ?></li>
+            <?php endwhile; ?>
+          <?php else : ?>
+            <?php foreach ( $order as $key => $item ) : ?>
+              <li class="item"><?= $item['text'] ?> </li>
+            <?php endforeach; ?>
+          <?php endif; ?>
+        </ul>
+      </div>
+
+      <div class="top-screen--bottom">
+        <div class="top-screen__images--top">
+          <img 
+            src="<?= bloginfo('template_url') . '/images/loader.gif' ?>" 
+            data-src="<?= get_sub_field('team_top_1')['url'] ? get_sub_field('team_top_1')['url'] : bloginfo('template_url') . '/images/page-team/team1.webp' ?>"
+            class="lazy"
+            width="1px"
+            height="1px"
+          />
+        </div>
+        <div class="top-screen__images--bottom">
+          
+          <img 
+            src="<?= bloginfo('template_url') . '/images/loader.gif' ?>" 
+            data-src="<?= get_sub_field('team_top_2')['url'] ? get_sub_field('team_top_2')['url'] : bloginfo('template_url') . '/images/page-team/team2.webp' ?>"
+            class="lazy"
+            width="1px"
+            height="1px"
+          />
+          <img 
+            src="<?= bloginfo('template_url') . '/images/loader.gif' ?>" 
+            data-src="<?= get_sub_field('team_top_3')['url'] ? get_sub_field('team_top_3')['url'] : bloginfo('template_url') . '/images/page-team/team3.webp' ?>"
+            class="lazy"
+            width="1px"
+            height="1px"
+          />
+          <img 
+            src="<?= bloginfo('template_url') . '/images/loader.gif' ?>" 
+            data-src="<?= bloginfo('template_url') . '/images/page-team/team4.webp' ?>"
+            class="lazy"
+            width="1px"
+            height="1px"
+          />
+        </div>
+        
+      </div>
     </div>
   </div>
 </section>
